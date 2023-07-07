9d1f5c82468989eb5ef7c4764a278cd9b2d0477d
revol-xut <revol-xut@protonmail.com>
2023-07-08 01:10:50 +0200

    setting robot.txt with nginx

--- modules/management/website.nix
+++ modules/management/website.nix
@@ -6,9 +6,14 @@
       "${config.deployment-dd-ix.domain}" = {
         enableACME = true;
         forceSSL = true;
-        locations."/" = {
-          root = "${pkgs.dd-ix-website}/bin/";
-          tryFiles = "$uri /$1/index.html =404";
+        locations = {
+          "/" = {
+            root = "${pkgs.dd-ix-website}/bin/";
+            tryFiles = "$uri /$1/index.html =404";
+          };
+          "robot.txt" = {
+            root = ../../resources/;
+          };
         };
       };
     };
