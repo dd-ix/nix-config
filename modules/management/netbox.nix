10f5b591c9a54e1710cd6d639ccc3b9a273e26ef
Melo <sofia.eroshevich@snowbyte.team>
2023-08-11 20:54:26 +0200

    added netbox

---
+++ modules/management/netbox.nix
@@ -0,0 +1,37 @@
+{ config, pkgs, ... }:
+let
+  domain = "netbox.dd-ix.net";
+in
+{
+  sops.secrets.netbox_db_pass.owner = "netbox";
+  sops.secrets.netbox_secret_key_file.owner = "netbox";
+  
+
+  services.postgresql = {
+    enable = true;
+    ensureUsers = [
+      {
+        name = "netbox";
+        ensurePermissions = {
+          "DATABASE netbox" = "ALL PRIVILEGES";
+        };
+      }
+    ];
+    ensureDatabases = [ "netbox" ];
+  };
+
+  services.netbox = {
+    enable = true;
+    package = pkgs.netbox;
+    port = 9502;
+    listenAddress = "127.0.0.1";
+    secretKeyFile = "${config.sops.secrets.netbox_secret_key_file.path}";
+    
+  };
+
+  services.nginx.virtualHosts."netbox.dd-ix.net".locations."/".proxyPass = "http://127.0.0.1:9502"; 
+  services.nginx.virtualHosts."netbox.dd-ix.net".forceSSL = true;
+  services.nginx.virtualHosts."netbox.dd-ix.net".enableACME = true;
+
+  networking.firewall.allowedTCPPorts = [ 80 443 ];
+}
