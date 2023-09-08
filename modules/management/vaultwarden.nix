02cdf3c9f8d6bea0077b51ff7d0c964ddf7d7665
Melo <sofia.eroshevich@snowbyte.team>
2023-09-08 15:33:07 +0200

    fixed Vaultwarden

--- modules/management/vaultwarden.nix
+++ modules/management/vaultwarden.nix
@@ -1,26 +1,58 @@
 { pkgs, config, lib, ... }: {
 
-services.vaultwarden.enable = true;
-services.vaultwarden.config = {
-  DOMAIN = "https://vaultwarden.dd-ix.net";
-  SIGNUPS_ALLOWED = false;
-services.vaultwarden.dbBackend = "postgresql";
-services.vaultwarden.environmentFile = /var/lib/vaultwarden.env;
+  sops.secrets.postgres_vaultwarden.owner = config.services.postgresql.superUser;
 
-services.nginx = {
-    enable = true;
-    recommendedProxySettings = true;
-    # Use recommended settings
-    recommendedGzipSettings = true;
+  services = {
+    postgresql = {
+      enable = true;
+      ensureUsers = [
+        {
+          name = "vaultwarden";
+          ensurePermissions = {
+            "DATABASE vaultwarden" = "ALL PRIVILEGES";
+          };
+        }
+      ];
+      ensureDatabases = [ "vaultwarden" ];
+    };
+    vaultwarden = {
+      enable = true;
+      config = {
+        ROCKET_ADDRESS = "127.0.0.1";
+        ROCKET_PORT = 8222;
+        DOMAIN = "https://vaultwarden.dd-ix.net";
+        SIGNUPS_ALLOWED = false;
+      };
+      dbBackend = "postgresql";
+      environmentFile = /var/lib/vaultwarden.env;
+    };
 
-    virtualHosts."vautwarden.dd-ix.net" = {
-      enableACME = true;
-      forceSSL = true;
-      locations."/" = {
-        proxyPass = "http://127.0.0.1:8000";
-        proxyWebsockets = true;
+    nginx = {
+      enable = true;
+      recommendedProxySettings = true;
+      # Use recommended settings
+      recommendedGzipSettings = true;
+
+      virtualHosts."vautwarden.dd-ix.net" = {
+        enableACME = true;
+        forceSSL = true;
+        locations."/" = {
+          proxyPass = "http://127.0.0.1:8222";
+          proxyWebsockets = true;
         };
       };
     };
+  };
+  systemd.services.vaultwarden-pgsetup = {
+    description = "Prepare postgres database";
+    wantedBy = [ "multi-user.target" ];
+    after = [ "networking.target" "postgresql.service" ];
+    serviceConfig.Type = "oneshot";
+
+    path = [ pkgs.sudo config.services.postgresql.package ];
+    script = ''
+      # create postgres user with the specified password
+      sudo -u ${config.services.postgresql.superUser} psql -c "ALTER ROLE vaultwarden WITH PASSWORD '$(cat ${config.sops.secrets.postgres_vaultwarden.path})"
+    '';
   };
 }
