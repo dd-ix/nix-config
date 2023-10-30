43bd3018b0153739090abd2e893d8e312b7804b0
Marcel <me@m4rc3l.de>
2023-10-30 21:25:39 +0100

    Fixed bookstack keycloak integragtion

--- modules/management/bookstack.nix
+++ modules/management/bookstack.nix
@@ -31,21 +31,15 @@
         AUTH_METHOD = "oidc";
         AUTH_AUTO_INITIATE = true;
         OIDC_NAME = ''"DD-IX Bookstack"'';
+        OIDC_DISPLAY_NAME_CLAIMS = "name";
         OIDC_CLIENT_ID = "bookstack";
-        OIDC_CLIENT_SECRET._secret = config.sops.secrets.bookstack_oidc_secret.path;
-        OIDC_ISSUER = "https://keycloak.dd-ix.net/";
-        OIDC_AUTH_ENDPOINT = "https://keycloak.dd-ix.net/realms/DD-IX/protocol/openid-connect/auth";
-        OIDC_TOKEN_ENDPOINT = "https://keycloak.dd-ix.net/realms/DD-IX/protocol/openid-connect/token";
+        OIDC_CLIENT_SECRET._secret = "${config.sops.secrets.bookstack_oidc_secret.path}";
+        OIDC_ISSUER = "https://keycloak.dd-ix.net/realms/DD-IX";
         OIDC_ISSUER_DISCOVER = true;
-        OIDC_USER_TO_GROUPS = true;
-        OIDC_GROUPS_CLAIM = "groups";
-        OIDC_ADDITIONAL_SCOPES = "groups";
-        OIDC_REMOVE_FROM_GROUPS = true;
 
-        # does not work yet, requires newer bookstash version
-        # OIDC_USER_TO_GROUPS = true;
-        # OIDC_GROUPS_CLAIM = "groups";
-        # OIDC_REMOVE_FROM_GROUPS = true;
+        OIDC_USER_TO_GROUPS = true;
+        OIDC_GROUPS_CLAIM = "resource_access.bookstack.roles";
+        OIDC_REMOVE_FROM_GROUPS = true;
       };
     };
     mysql = {
