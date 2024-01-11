5e0fe6cd6b8e4090bed0e5a17c537472b294ea6d
Marcel <me@m4rc3l.de>
2024-01-11 17:36:44 +0100

    pr feedback

--- modules/management/bind9.nix
+++ modules/management/bind9.nix
@@ -5,19 +5,19 @@ let
   ibh_ans_ip = [
     # ans-01.ibh.de
     "212.111.228.50"
-    #"2a01:7700:0:1035::1:50"
+    "2a01:7700:0:1035::1:50"
     # ans-02.ibh.net
     "193.36.123.50"
-    #"2a01:7700:0:1036::1:50"
+    "2a01:7700:0:1036::1:50"
     # ans-03.ibh.de
     "195.30.105.203"
-    #"2001:608:c00:10::1:138"
+    "2001:608:c00:10::1:138"
     # ans-04.ibh.services
     "167.235.139.88"
-    #"2a01:4f8:c0c:74b9::1"
+    "2a01:4f8:c0c:74b9::1"
     # ans-05.ibh.net
     "65.109.1.68"
-    #"2a01:4f9:c012:61fd::1"
+    "2a01:4f9:c012:61fd::1"
   ];
 in
 {
@@ -33,23 +33,14 @@ in
         master = true;
         file = self + "/resource/dd-ix.net.zone";
         slaves = ibh_ans_ip;
-        allowQuery = ibh_ans_ip ++ ["127.0.0.0/8" "::1/128"];
       };
-      # reverse zone for IX IPv4
-      #"120.36.193.in-addr.arpa" = {
-      #  master = true;
-      #  file = "${file_dir}/120.36.193.in-addr.arpa";
-      #  slaves = ibh_ans_ip;
-      #  allowQuery = ibh_ans_ip ++ [ "127.0.0.0/8" "::1/128" ];
-      #};
 
-      # reverse zone for IX IPv6
-      #"8.1.d.d.9.1.0.0.8.f.7.0.1.0.0.2.ip6.arpa" = {
-      #  master = true;
-      #  file = "${file_dir}/8.1.d.d.9.1.0.0.8.f.7.0.1.0.0.2.ip6.arpa";
-      #  slaves = ibh_ans_ip;
-      #  allowQuery = ibh_ans_ip ++ [ "127.0.0.0/8" "::1/128" ];
-      #};
+      # ipv6 pa
+      "0.b.0.8.0.0.7.7.1.0.a.2.ip6.arpa" = {
+        master = true;
+        file = self + "/resource/0.b.0.8.0.0.7.7.1.0.a.2.ip6.arpa.zone";
+        slaves = ibh_ans_ip;
+      };
     };
 
     extraOptions = ''
