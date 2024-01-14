88fe5f288da26401104a0ab2139ed19197db1edb
Marcel <me@m4rc3l.de>
2024-01-14 11:15:59 +0100

    Added bridge interfaces

--- hosts/mno001/network.nix
+++ hosts/mno001/network.nix
@@ -37,8 +37,40 @@ in
         bondConfig = {
           Mode = "802.3ad"; # LACP 
           MIIMonitorSec = "250ms";
-          LACPTransmitRate = "fast";
+          LACPTransmitRate = "fast"; 
         };
+      };
+
+      # 20-svc-internet
+      "20-uplink" = {
+        netdevConfig = {
+          # svc-internet
+          Name = "uplink";
+          Kind = "vlan";
+        };
+        vlanConfig = {
+          Id = 100;
+        };
+      };
+
+      "20-br-svc-internet".netdevConfig = {
+        Name = "br-svc-internet";
+        Kind = "bridge";
+      };
+
+      "20-svc-services" = {
+        netdevConfig = {
+          Name = "svc-services";
+          Kind = "vlan";
+        };
+        vlanConfig = {
+          Id = 101;
+        };
+      };
+
+      "20-br-svc-services".netdevConfig = {
+        Name = "br-svc-services";
+        Kind = "bridge";
       };
 
       "20-svc-management" = {
@@ -51,19 +83,9 @@ in
         };
       };
 
-      "20-uplink" = {
-        netdevConfig = {
-          Name = "uplink";
-          Kind = "vlan";
-        };
-        vlanConfig = {
-          Id = 100;
-        };
-      };
-
-      "30-microvm-inet".netdevConfig = {
+      "20-br-svc-management".netdevConfig = {
+        Name = "br-svc-management";
         Kind = "bridge";
-        Name = "microvm-inet";
       };
     };
 
@@ -72,26 +94,6 @@ in
         matchConfig.Name = "${bond_device_name}";
 
         vlan = [ "uplink" "svc-management" ];
-
-        networkConfig = {
-          DHCP = "no";
-        };
-      };
-
-      "10-svc-management" = {
-        matchConfig.Name = "svc-management";
-        address = [ "2a01:7700:80b0:7000::1/64" ];
-      };
-
-      "10-uplink" = {
-        matchConfig.Name = "uplink";
-
-        address = [ "212.111.245.178/29" ];
-        routes = [
-          { routeConfig.Gateway = "212.111.245.177"; }
-        ];
-
-        vlan = [ "uplink" ];
 
         networkConfig = {
           DHCP = "no";
@@ -112,9 +114,54 @@ in
         };
       };
 
-      "30-microvm-inet" = {
+      #"10-svc-internet" = {
+      #  matchConfig.Name = "svc-internet";
+      #  networkConfig.Bridge = "br-svc-internet";
+      #};
+
+      #10-br-svc-internet
+      "10-uplink" = {
+        #10-br-svc-internet
+        matchConfig.Name = "uplink";
+
+        address = [ "212.111.245.178/29" ];
+        routes = [
+          { routeConfig.Gateway = "212.111.245.177"; }
+        ];
+
+        # remove
+        vlan = [ "uplink" ];
+
+        # remove (is default)
+        networkConfig = {
+          DHCP = "no";
+        };
+      };
+
+      "30-microvm-br-svc-internet" = {
         matchConfig.Name = "vm-inet-*";
-        networkConfig.Bridge = "microvm-inet";
+        networkConfig.Bridge = "br-svc-internet";
+      };
+
+      "10-svc-services" = {
+        matchConfig.Name = "svc-services";
+        networkConfig.Bridge = "br-svc-services";
+      };
+
+      "30-microvm-br-svc-services" = {
+        matchConfig.Name = "vm-srv-*";
+        networkConfig.Bridge = "br-svc-services";
+      };
+
+      "10-svc-management" = {
+        matchConfig.Name = "svc-management";
+        networkConfig.Bridge = "br-svc-management";
+      };
+
+      "10-br-svc-management" = {
+        matchConfig.Name = "br-svc-management";
+        address = [ "2a01:7700:80b0:7000::1/64" ];
+        #routes = [ { routeConfig.Gateway = "fe80::defa"; } ];
       };
     };
   };
