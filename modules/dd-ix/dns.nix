45b4f00e4ee9c0a1e63b36340f0c675ec17a3cb7
Marcel <me@m4rc3l.de>
2024-01-31 14:01:01 +0100

    Removed v4 dns to fix acme

--- modules/dd-ix/dns.nix
+++ modules/dd-ix/dns.nix
@@ -3,10 +3,8 @@
   networking.nameservers = [
     # rns-01.ibh.net
     "2a01:7700:0:1035::1:53"
-    "212.111.228.53"
     # rns-02.ibh.net
     "2a01:7700:0:1036::1:53"
-    "193.36.123.53"
   ];
 
   services.resolved = {
