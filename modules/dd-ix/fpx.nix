9fe3c706b1e39a4e8219199a60230c5c16862674
Marcel <me@m4rc3l.de>
2024-02-03 02:19:09 +0100

    fixed proxy

--- modules/dd-ix/fpx.nix
+++ modules/dd-ix/fpx.nix
@@ -6,6 +6,9 @@ in
   options.dd-ix.useFpx = lib.mkEnableOption (lib.mdDoc "dd-ix fpx");
 
   config = lib.mkIf cfg {
-    networking.proxy.default = "http://svc-fpx01.dd-ix.net:8080";
+    networking.proxy = {
+      default = "http://svc-fpx01.dd-ix.net:8080";
+      noProxy = "127.0.0.1/8,::1,2a01:7700:80b0::/48,localhost,dd-ix.net";
+    };
   };
 }
