d5ed299e941d29f4f2bfcb13891aaac4e69070f6
Marcel <me@m4rc3l.de>
2024-02-04 15:59:00 +0100

    fixed listmonk

--- modules/management/listmonk.nix
+++ modules/management/listmonk.nix
@@ -9,8 +9,8 @@ let
     }' WHERE key = '${key}';";
   updateDatabaseConfigSQL = pkgs.writeText "update-database-config.sql"
     (lib.concatStringsSep "\n" (lib.mapAttrsToList setDatabaseOption
-      (if (cfg.database.settings != null) then
-        cfg.database.settings
+      (if (cfg.database.settings2 != null) then
+        cfg.database.settings2
       else
         { })));
 
@@ -32,87 +32,112 @@ let
   '';
 in
 {
-  sops.secrets."lists_env" = {
-    sopsFile = self + "/secrets/management/lists.yaml";
-  };
-
-  sops.secrets."lists_bounce_migadu" = {
-    sopsFile = self + "/secrets/management/lists.yaml";
-  };
-
-  systemd.services.listmonk = {
-    serviceConfig = {
-      ExecStartPre = lib.mkForce [
-        ''${pkgs.coreutils}/bin/mkdir -p "''${STATE_DIRECTORY}/listmonk/uploads"''
-        "${cfg.package}/bin/listmonk --config ${cfgFile} --idempotent --install --upgrade --yes"
-        "${updateBounceSettings}/bin/update-database-config.sh"
-      ];
-      LoadCredential = "migadu_bounce:${config.sops.secrets."lists_bounce_migadu".path}";
+  options = {
+    services.listmonk.database.settings2 = lib.mkOption {
+      default = null;
+      type = with lib.types; let
+        valueType = nullOr
+          (oneOf [
+            bool
+            int
+            float
+            str
+            path
+            (attrsOf valueType)
+            (listOf valueType)
+          ]) // {
+          description = "listmonk value";
+        };
+      in
+      valueType;
+      description = lib.mdDoc
+        "Dynamic settings in the PostgreSQL database, set by a SQL script, see <https://github.com/knadh/listmonk/blob/master/schema.sql#L177-L230> for details.";
     };
   };
-  services = {
-    listmonk = {
-      enable = true;
-      settings = {
-        app.admin_username = "admin";
-        db = {
-          host = "svc-pg01.dd-ix.net";
-          port = 5432;
-          user = "listmonk";
-          database = "listmonk";
-          ssl_mode = "verify-full";
-        };
+  config = {
+    sops.secrets."lists_env" = {
+      sopsFile = self + "/secrets/management/lists.yaml";
+    };
+
+    sops.secrets."lists_bounce_migadu" = {
+      sopsFile = self + "/secrets/management/lists.yaml";
+    };
+
+    systemd.services.listmonk = {
+      serviceConfig = {
+        ExecStartPre = lib.mkForce [
+          ''${pkgs.coreutils}/bin/mkdir -p "''${STATE_DIRECTORY}/listmonk/uploads"''
+          "${cfg.package}/bin/listmonk --config ${cfgFile} --idempotent --install --upgrade --yes"
+          "${updateBounceSettings}/bin/update-database-config.sh"
+        ];
+        LoadCredential = "migadu_bounce:${config.sops.secrets."lists_bounce_migadu".path}";
       };
-      secretFile = config.sops.secrets."lists_env".path;
-      database = {
-        createLocally = false;
-        mutableSettings = false;
+    };
+    services = {
+      listmonk = {
+        enable = true;
         settings = {
-          "app.site_name" = "DD-IX Malling";
-          "app.root_url" = "https://lists.dd-ix.net";
-          "app.logo_url" = "https://dd-ix.net/en/assets/images/logo.svg";
-          "app.favicon_url" = "https://dd-ix.net/en/favicon.ico";
-          "app.from_email" = "DD-IX Mailling <noreply@lists.dd-ix.net>";
-          "privacy.domain_blocklist" = [ ]; # list of domains excluded from subscribing
-          "app.notify_emails" = [ "noc@dd-ix.net" ];
-          smtp = [{
-            enabled = true;
-            host = "mta.dd-ix.net";
-            port = 25;
-            uuid = "3c860444-42f3-425a-8ce7-36aebb7add95";
-            tls_type = "none";
-            username = "";
-            idle_timeout = "15s";
-            wait_timeout = "5s";
-            auth_protocol = "none";
-            email_headers = [ ];
-            hello_hostname = "";
-            max_msg_retries = 2;
-            tls_skip_verify = false;
-          }];
+          app.admin_username = "admin";
+          db = {
+            host = "svc-pg01.dd-ix.net";
+            port = 5432;
+            user = "listmonk";
+            database = "listmonk";
+            ssl_mode = "verify-full";
+          };
+        };
+        secretFile = config.sops.secrets."lists_env".path;
+        database = {
+          createLocally = false;
+          mutableSettings = false;
+          settings2 = {
+            "app.site_name" = "DD-IX Mailling";
+            "app.root_url" = "https://lists.dd-ix.net";
+            "app.logo_url" = "https://dd-ix.net/en/assets/images/logo.svg";
+            "app.favicon_url" = "https://dd-ix.net/en/favicon.ico";
+            "app.from_email" = "DD-IX Mailling <noreply@lists.dd-ix.net>";
+            "privacy.domain_blocklist" = [ ]; # list of domains excluded from subscribing
+            "app.notify_emails" = [ "noc@dd-ix.net" ];
+            smtp = [{
+              enabled = true;
+              host = "mta.dd-ix.net";
+              port = 25;
+              uuid = "3c860444-42f3-425a-8ce7-36aebb7add95";
+              tls_type = "none";
+              username = "";
+              idle_timeout = "15s";
+              wait_timeout = "5s";
+              auth_protocol = "none";
+              email_headers = [ ];
+              hello_hostname = "";
+              max_msg_retries = 2;
+              tls_skip_verify = false;
+              max_conns = 1;
+
+            }];
+          };
         };
       };
-    };
+      nginx = {
+        enable = true;
+        virtualHosts."lists.${config.deployment-dd-ix.domain}" = {
+          listen = [{
+            addr = "[::]:443";
+            proxyProtocol = true;
+            ssl = true;
+          }];
 
-    nginx = {
-      enable = true;
-      virtualHosts."lists.${config.deployment-dd-ix.domain}" = {
-        listen = [{
-          addr = "[::]:443";
-          proxyProtocol = true;
-          ssl = true;
-        }];
+          onlySSL = true;
+          useACMEHost = "lists.${config.deployment-dd-ix.domain}";
 
-        onlySSL = true;
-        useACMEHost = "lists.${config.deployment-dd-ix.domain}";
-
-        locations =
-          let
-            cfg = config.services.listmonk.settings;
-          in
-          {
-            "/".proxyPass = "http://${cfg.app.address}";
-          };
+          locations =
+            let
+              cfg = config.services.listmonk.settings;
+            in
+            {
+              "/".proxyPass = "http://${cfg.app.address}";
+            };
+        };
       };
     };
   };
