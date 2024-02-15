1c4468adf194873c3971fbc083702f1c3824e7de
Marcel <me@m4rc3l.de>
2024-02-15 22:44:46 +0100

    fixed alice footer

--- resources/alice/config.js
+++ resources/alice/config.js
@@ -3,10 +3,7 @@ Alice.updateContent({
       title: "DD-IX Looking Glass",
       tagline: "powered by Alice, your friendly BIRD Looking Glass"
   },
-  footer: "<div class=\"ddix-footer\">" +
-    "<a href='https://dd-ix.net/imprint'>Imprint</a>" +
-    "<a href='https://dd-ix.net/privacy-policy'>Privacy Policy</a>" +
-  "</div>"
+  footer: "<a href='https://dd-ix.net/imprint'>Imprint</a> <a href='https://dd-ix.net/privacy-policy'>Privacy Policy</a>"
 });
 
 document.title = "BCIX Looking Glass";
