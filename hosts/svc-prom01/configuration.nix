c18901284ab7aaffe0aa2afa37ca8847aa047443
Marcel <me@m4rc3l.de>
2024-02-21 22:36:31 +0100

    Added prometheus

---
+++ hosts/svc-prom01/configuration.nix
@@ -0,0 +1,24 @@
+{ ... }:
+{
+  dd-ix = {
+    microvm = {
+      enable = true;
+
+      mem = 1024 * 2;
+      vcpu = 2;
+
+      hostName = "svc-prom01";
+      mac = "42:b3:bc:bb:11:b6";
+      vlan = "a";
+
+      v6Addr = "2a01:7700:80b0:7002::4/64";
+    };
+
+    acme = [{
+      name = "svc-prom01.dd-ix.net";
+      group = "nginx";
+    }];
+  };
+
+  system.stateVersion = "23.11";
+}
---
+++ modules/obs/prometheus.nix
@@ -0,0 +1,26 @@
+{ config, ... }:
+{
+  services.prometheus = {
+    enable = true;
+    webExternalUrl = "https://svc-prom01.dd-ix.net";
+  };
+
+  services.nginx = {
+    enable = true;
+    virtualHosts."svc-prom01.dd-ix.net" = {
+      listen = [{
+        addr = "[::]:443";
+        proxyProtocol = true;
+        ssl = true;
+      }];
+
+      onlySSL = true;
+      useACMEHost = "svc-prom01.dd-ix.net";
+
+      locations."/" = {
+        proxyWebsockets = true;
+        proxyPass = "http://${config.services.prometheus.listenAddress}:${toString config.services.prometheus.port}";
+      };
+    };
+  };
+}
