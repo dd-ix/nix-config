a6c43077e3e75d605a0f5b8ffdd9eb7e2e84dd79
Marcel <me@m4rc3l.de>
2024-04-05 22:07:03 +0200

    added ixp-user for web01

--- hosts/svc-web01/configuration.nix
+++ hosts/svc-web01/configuration.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ pkgs, ... }:
 let
   addr = "2a01:7700:80b0:6001::13";
 in
@@ -47,6 +47,27 @@ in
       enable = true;
     };
   };
+
+  users.users.ixp-deploy = {
+    isNormalUser = true;
+    openssh.authorizedKeys = {
+      keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIH0vuZeitSJiVxdACcwB8s1Cj2hi0wXjDMbhLelEJmIv"
+      ];
+      keyFiles = [
+        ../../keys/ssh/tassilo
+        ../../keys/ssh/melody
+        ../../keys/ssh/fiasko
+        ../../keys/ssh/marcel
+        ../../keys/ssh/adb
+      ];
+    };
+  };
+
+  environment.systemPackages = with pkgs; [
+    # ansible trash:
+    python3
+  ];
 
   system.stateVersion = "23.11";
 }
