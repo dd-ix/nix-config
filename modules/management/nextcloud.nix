78f87c3da86f660039012a8b82d8d98738c4c478
Marcel <me@m4rc3l.de>
2024-06-07 21:30:07 +0200

    nextcloud: cleanup old onlyoffice options

--- modules/management/nextcloud.nix
+++ modules/management/nextcloud.nix
@@ -3,10 +3,6 @@ let
   domain = "cloud.${config.dd-ix.domain}";
 in
 {
-  nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
-    "corefonts"
-  ];
-
   sops.secrets."cloud_admin_pw" = {
     sopsFile = self + "/secrets/management/cloud.yaml";
     owner = config.systemd.services.nextcloud-setup.serviceConfig.User;
@@ -17,98 +13,12 @@ in
     owner = config.systemd.services.nextcloud-setup.serviceConfig.User;
   };
 
-  #sops.secrets."office_db_pw" = {
-  #  sopsFile = self + "/secrets/management/cloud.yaml";
-  #  owner = config.systemd.services.onlyoffice-docservice.serviceConfig.User;
-  #};
-
-  #sops.secrets."office_jwt_secret" = {
-  #  sopsFile = self + "/secrets/management/cloud.yaml";
-  #  owner = config.systemd.services.onlyoffice-docservice.serviceConfig.User;
-  #};
-
   sops.secrets."office_env" = {
     sopsFile = self + "/secrets/management/cloud.yaml";
     owner = "root";
   };
 
-  systemd.services = {
-    nextcloud-setup.after = [ "network.target" ];
-    #onlyoffice-converter = {
-    #  after = lib.mkForce [ "network.target" "onlyoffice-docservice.service" ];
-    #  requires = lib.mkForce [ "network.target" "onlyoffice-docservice.service" ];
-    #};
-    #onlyoffice-docservice = {
-    #  after = [ "network.target" ];
-    #  requires = lib.mkForce [ ];
-    #  serviceConfig.ExecStartPre = lib.mkForce [
-    #    (pkgs.writeShellScript
-    #      "onlyoffice-prestart"
-    #      ''
-    #        PATH=$PATH:${lib.makeBinPath (with pkgs; [ jq moreutils config.services.postgresql.package ])}
-    #        umask 077
-    #        mkdir -p /run/onlyoffice/config/ /var/lib/onlyoffice/documentserver/sdkjs/{slide/themes,common}/ /var/lib/onlyoffice/documentserver/{fonts,server/FileConverter/bin}/
-    #        cp -r ${cfg.package}/etc/onlyoffice/documentserver/* /run/onlyoffice/config/
-    #       chmod u+w /run/onlyoffice/config/default.json
-    #
-    #           # Allow members of the onlyoffice group to serve files under /var/lib/onlyoffice/documentserver/App_Data
-    #           chmod g+x /var/lib/onlyoffice/documentserver
-    #
-    #            cp /run/onlyoffice/config/default.json{,.orig}
-    #
-    #            # for a mapping of environment variables from the docker container to json options see
-    # https://github.com/ONLYOFFICE/Docker-DocumentServer/blob/master/run-document-server.sh
-    #            jq '
-    #              .services.CoAuthoring.server.port = ${toString cfg.port} |
-    #              .services.CoAuthoring.sql.dbHost = "${cfg.postgresHost}" |
-    #              .services.CoAuthoring.sql.dbName = "${cfg.postgresName}" |
-    #            ${lib.optionalString (cfg.postgresPasswordFile != null) ''
-    #             .services.CoAuthoring.sql.dbPass = "'"$(cat ${cfg.postgresPasswordFile})"'" |
-    #            ''}
-    #              .services.CoAuthoring.sql.dbUser = "${cfg.postgresUser}" |
-    #            ${lib.optionalString (cfg.jwtSecretFile != null) ''
-    #              .services.CoAuthoring.sql.pgPoolExtraOptions.ssl = true |
-    #              .services.CoAuthoring.token.enable.browser = true |
-    #              .services.CoAuthoring.token.enable.request.inbox = true |
-    #              .services.CoAuthoring.token.enable.request.outbox = true |
-    #              .services.CoAuthoring.secret.inbox.string = "'"$(cat ${cfg.jwtSecretFile})"'" |
-    #              .services.CoAuthoring.secret.outbox.string = "'"$(cat ${cfg.jwtSecretFile})"'" |
-    #              .services.CoAuthoring.secret.session.string = "'"$(cat ${cfg.jwtSecretFile})"'" |
-    #            ''}
-    #             .rabbitmq.url = "${cfg.rabbitmqUrl}"
-    #              ' /run/onlyoffice/config/default.json | sponge /run/onlyoffice/config/default.json
-    #
-    #            export PGPASSWORD=$(cat ${cfg.postgresPasswordFile})
-    ##            if psql \
-    #                --host svc-pg01.dd-ix.net \
-    #                --port 5432 \
-    #               --username onlyoffice \
-    #               --dbname onlyoffice \
-    ##               --command "SELECT 'task_result'::regclass;" >/dev/null; then
-    #             psql \
-    #              --host svc-pg01.dd-ix.net \
-    #              --port 5432 \
-    ##              --username onlyoffice \
-    #              --dbname onlyoffice \
-    #             -f "${cfg.package}/var/www/onlyoffice/documentserver/server/schema/postgresql/removetbl.sql"
-    ##           psql \
-    #             --host svc-pg01.dd-ix.net \
-    #            --port 5432 \
-    ##            --username onlyoffice \
-    #            --dbname onlyoffice \
-    #           -f "${cfg.package}/var/www/onlyoffice/documentserver/server/schema/postgresql/createdb.sql"
-    ##       else
-    #         psql \
-    ##          --host svc-pg01.dd-ix.net \
-    #          --port 5432 \
-    #          --username onlyoffice \
-    ##         --dbname onlyoffice \
-    #         -f "${cfg.package}/var/www/onlyoffice/documentserver/server/schema/postgresql/createdb.sql"
-    #    fi
-    ##  '')
-    #];
-    # };
-  };
+  systemd.services.nextcloud-setup.after = [ "network.target" ];
 
   services = {
     postgresql = {
@@ -156,17 +66,6 @@ in
       extraAppsEnable = true;
     };
 
-    #onlyoffice = {
-    #  enable = false;
-    #  package = nixpkgs-onlyoffice.onlyoffice-documentserver;
-    #  hostname = "office.${config.dd-ix.domain}";
-    #  postgresName = "onlyoffice"; # dbname
-    #  postgresHost = "svc-pg01.dd-ix.net";
-    #  postgresUser = "onlyoffice";
-    #  postgresPasswordFile = config.sops.secrets."office_db_pw".path;
-    #  jwtSecretFile = config.sops.secrets."office_jwt_secret".path;
-    #};
-
     nginx.virtualHosts = {
       "cloud.${config.dd-ix.domain}" = {
         listen = [{
@@ -195,13 +94,9 @@ in
     };
   };
 
-  virtualisation = {
-    oci-containers = {
-      containers.onlyoffice = {
-        image = "onlyoffice/documentserver:8.0.1.1";
-        environmentFiles = [ config.sops.secrets."office_env".path ];
-        extraOptions = [ "--network=host" ];
-      };
-    };
+  virtualisation.oci-containers.containers.onlyoffice = {
+    image = "onlyoffice/documentserver:8.0.1.1";
+    environmentFiles = [ config.sops.secrets."office_env".path ];
+    extraOptions = [ "--network=host" ];
   };
 }
