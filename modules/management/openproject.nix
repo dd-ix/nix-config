d25fda54422bb879886e1b120ae9c2da969ea2ad
Marcel <me@m4rc3l.de>
2024-06-20 22:30:56 +0200

    orga: format

--- modules/management/openproject.nix
+++ modules/management/openproject.nix
@@ -1,16 +1,23 @@
 { self, config, lib, ... }:
 
 let
-  env = {
-    IMAP_ENABLED = "false";
-    OPENPROJECT_CACHE__MEMCACHE__SERVER = "127.0.0.1:11211";
-    OPENPROJECT_HOST__NAME = "orga.dd-ix.net";
-    OPENPROJECT_HSTS = "true";
-    OPENPROJECT_HTTPS = "true";
-    OPENPROJECT_RAILS__RELATIVE__URL__ROOT = "";
-    RAILS_CACHE_STORE = "memcache";
-    RAILS_MAX_THREADS = "16";
-    RAILS_MIN_THREADS = "4";
+  base = {
+    image = "openproject/openproject:14-slim";
+    extraOptions = [ "--network=host" ];
+
+    environmentFiles = [ config.sops.secrets."openproject_env".path ];
+
+    environment = {
+      IMAP_ENABLED = "false";
+      OPENPROJECT_CACHE__MEMCACHE__SERVER = "127.0.0.1:11211";
+      OPENPROJECT_HOST__NAME = "orga.dd-ix.net";
+      OPENPROJECT_HSTS = "true";
+      OPENPROJECT_HTTPS = "true";
+      OPENPROJECT_RAILS__RELATIVE__URL__ROOT = "";
+      RAILS_CACHE_STORE = "memcache";
+      RAILS_MAX_THREADS = "16";
+      RAILS_MIN_THREADS = "4";
+    };
   };
 in
 {
@@ -19,76 +26,67 @@ in
     owner = "root";
   };
 
-  services.nginx.enable = true;
   services.memcached.enable = true;
 
   systemd.services."podman-ddix-orga-seeder".serviceConfig.Restart = lib.mkForce "on-failure";
 
-  services.nginx.virtualHosts."orga.${config.dd-ix.domain}" = {
-    listen = [{
-      addr = "[::]:443";
-      proxyProtocol = true;
-      ssl = true;
-    }];
+  services.nginx = {
+    enable = true;
 
-    onlySSL = true;
-    useACMEHost = "orga.${config.dd-ix.domain}";
+    virtualHosts."orga.${config.dd-ix.domain}" = {
+      listen = [{
+        addr = "[::]:443";
+        proxyProtocol = true;
+        ssl = true;
+      }];
 
-    locations."/".proxyPass = "http://127.0.0.1:8080";
+      onlySSL = true;
+      useACMEHost = "orga.${config.dd-ix.domain}";
+
+      locations."/" = {
+        root = "/var/lib/openproject/public";
+        # just something that does not exists
+        index = "X6XewZMsmreGIxx1lCdp0Yo1X4qHTivW";
+        tryFiles = "$uri @website";
+        extraConfig = ''
+          expires max;
+          access_log off;
+        '';
+      };
+
+      locations."@website".proxyPass = "http://127.0.0.1:8080";
+    };
   };
 
-  virtualisation.oci-containers.containers."ddix-orga-cron" = {
-    image = "openproject/openproject:14-slim";
-    environment = env;
-    environmentFiles = [ config.sops.secrets."openproject_env".path ];
+  virtualisation.oci-containers.containers."ddix-orga-cron" = base // {
     volumes = [
       "/var/lib/openproject/assets:/var/openproject/assets:rw"
     ];
     cmd = [ "./docker/prod/cron" ];
-    extraOptions = [ "--network=host" ];
     dependsOn = [ "ddix-orga-seeder" ];
   };
 
-  virtualisation.oci-containers.containers."ddix-orga-seeder" = {
-    image = "openproject/openproject:14-slim";
-    environment = env;
-    environmentFiles = [ config.sops.secrets."openproject_env".path ];
+  virtualisation.oci-containers.containers."ddix-orga-seeder" = base // {
     volumes = [
       "/var/lib/openproject/assets:/var/openproject/assets:rw"
     ];
     cmd = [ "./docker/prod/seeder" ];
-    extraOptions = [ "--network=host" ];
   };
 
-  virtualisation.oci-containers.containers."ddix-orga-web" = {
-    image = "openproject/openproject:14-slim";
-    environment = env;
-    environmentFiles = [ config.sops.secrets."openproject_env".path ];
+  virtualisation.oci-containers.containers."ddix-orga-web" = base // {
     volumes = [
       "/var/lib/openproject/public:/app/public:rw"
       "/var/lib/openproject/assets:/var/openproject/assets:rw"
     ];
     cmd = [ "./docker/prod/web" ];
-    extraOptions = [
-      "--health-cmd='[\"curl\",\"-f\",\"http://localhost:8080/health_checks/default\"]'"
-      "--health-interval=10s"
-      "--health-retries=3"
-      "--health-start-period=30s"
-      "--health-timeout=3s"
-      "--network=host"
-    ];
     dependsOn = [ "ddix-orga-seeder" ];
   };
 
-  virtualisation.oci-containers.containers."ddix-orga-worker" = {
-    image = "openproject/openproject:14-slim";
-    environment = env;
-    environmentFiles = [ config.sops.secrets."openproject_env".path ];
+  virtualisation.oci-containers.containers."ddix-orga-worker" = base // {
     volumes = [
       "/var/lib/openproject/assets:/var/openproject/assets:rw"
     ];
     cmd = [ "./docker/prod/worker" ];
-    extraOptions = [ "--network=host" ];
     dependsOn = [ "ddix-orga-seeder" ];
   };
 }
