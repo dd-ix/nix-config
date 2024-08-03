4665da0a5c99131c66747c3f63c4142e68cff508
Marcel <me@m4rc3l.de>
2024-08-04 00:39:26 +0200

    post: add api token

--- modules/management/post.nix
+++ modules/management/post.nix
@@ -1,6 +1,10 @@
-{ self, ... }:
+{ self, config, ... }:
 
 {
+  sops.secrets."post_api_token" = {
+    sopsFile = self + "/secrets/management/mta.yaml";
+  };
+
   services.post = {
     enable = true;
     smtp = {
@@ -8,5 +12,6 @@
       port = 25;
     };
     templateGlob = self + "/resources/email_templates/*.html";
+    apiTokenFile = config.sops.secrets."post_api_token".path;
   };
 }
