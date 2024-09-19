0c4911d31b83e0aa87bf10ef1860d6ab455647c4
tanneberger <github@tanneberger.me>
2024-09-19 04:31:08 +0200

    adding svc-tix01

--- flake.nix
+++ flake.nix
@@ -439,6 +439,20 @@
             ./modules/dd-ix-microvm.nix
           ];
         };
+        svc-tix01 = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          specialArgs = { inherit inputs self; };
+          modules = [
+            self.nixosModules.common
+            ifstate.nixosModules.default
+            { nixpkgs.overlays = [ ifstate.overlays.default ]; }
+            microvm.nixosModules.microvm
+            sops-nix.nixosModules.default
+            ./hosts/svc-tix01
+            ./modules/dd-ix
+            ./modules/dd-ix-microvm.nix
+          ];
+        };
       };
   };
 }
--- modules/management/pretix.nix
+++ modules/management/pretix.nix
@@ -1,21 +1,20 @@
-{ config, pkgs, ... }:
-let
-  database_name = "pretix";
-  username = "pretix";
-in
-{
+{ config, pkgs, ... }: {
   services = {
     pretix = {
       enable = true;
       package = pkgs.pretix;
       settings = {
         database = {
-          name = database_name;
-          user = username;
+          name = "pretix";
+          user = "pretix";
+          host = "svc-pg01.dd-ix.net:5432";
+          backend = "postgresql";
         };
-      };
-      database = {
-        createLocally = true;
+        mail = {
+          host = "svc-mta01.dd-ix.net";
+          port = 25;
+          from = "pretix@lists.dd-ix.net";
+        };
       };
       nginx = {
         enable = true;
