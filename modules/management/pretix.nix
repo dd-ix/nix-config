0e4073b0b2c33e3971f377de85db39a8d621af74
Marcel <me@m4rc3l.de>
2024-09-19 07:34:29 +0200

    fixes

--- modules/management/pretix.nix
+++ modules/management/pretix.nix
@@ -1,25 +1,46 @@
-{ config, pkgs, ... }: {
+{ config, ... }:
+
+let
+  domain = "tickets.${config.dd-ix.domain}";
+in
+{
   services = {
     pretix = {
       enable = true;
-      package = pkgs.pretix;
       settings = {
+        url = "https://${domain}";
+        instance_name = domain;
         database = {
           name = "pretix";
           user = "pretix";
           host = "svc-pg01.dd-ix.net:5432";
           backend = "postgresql";
-          createLocally = false; # we want to use the postgres vm
+          createLocally = false;
         };
         mail = {
           host = "svc-mta01.dd-ix.net";
           port = 25;
-          from = "pretix@lists.dd-ix.net";
+          from = "noreply@tickets.dd-ix.net";
         };
       };
       nginx = {
         enable = true;
-        domain = "tickets.${config.dd-ix.domain}";
+        inherit domain;
+      };
+    };
+    nginx = {
+      enable = true;
+      virtualHosts = {
+        "${domain}" = {
+          listen = [{
+            addr = "[::]:443";
+            proxyProtocol = true;
+            ssl = true;
+          }];
+
+          onlySSL = true;
+          useACMEHost = domain;
+        };
       };
     };
   };
