67644e233cc93caadc8495ddde4c2ad3621e0d85
Marcel <me@m4rc3l.de>
2024-11-21 12:20:12 +0100

    nix: reduce duplicated flakes

--- flake.nix
+++ flake.nix
@@ -5,9 +5,12 @@
     nixpkgs.url = "github:NuschtOS/nuschtpkgs/backports-24.05";
     nixpkgs-unstable.url = "github:NuschtOS/nuschtpkgs/backports-24.11";
 
+    flake-utils.url = "github:numtide/flake-utils";
+
     ifstate = {
       url = "git+https://codeberg.org/m4rc3l/ifstate.nix";
       inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "flake-utils";
     };
 
     sops-nix = {
@@ -18,16 +21,19 @@
     microvm = {
       url = "github:astro/microvm.nix";
       inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "flake-utils";
     };
 
     website = {
       url = "github:dd-ix/website";
       inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "flake-utils";
     };
 
     website-content-api = {
       url = "github:dd-ix/website-content-api";
       inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "flake-utils";
     };
 
     website-content = {
@@ -38,31 +44,37 @@
     ixp-manager = {
       url = "github:dd-ix/ixp-manager.nix";
       inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "flake-utils";
     };
 
     authentik = {
       url = "github:nix-community/authentik-nix";
       inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "flake-utils";
     };
 
     ddix-ansible-ixp = {
       url = "github:dd-ix/ddix-ansible-ixp";
       inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "flake-utils";
     };
 
     sflow-exporter = {
       url = "github:dd-ix/sflow_exporter";
       inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "flake-utils";
     };
 
     post = {
       url = "github:dd-ix/post";
       inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "flake-utils";
     };
 
     nixos-modules = {
       url = "github:NuschtOS/nixos-modules";
       inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "flake-utils";
     };
   };
 
