5d9e315a81294c11ab3f5b3bf745efa9e99d9241
Marcel <me@m4rc3l.de>
2024-12-07 18:09:52 +0100

    obs: fix dcim alert

--- modules/obs/prometheus-all.nix
+++ modules/obs/prometheus-all.nix
@@ -94,7 +94,7 @@ in
           targets = [
             "https://dd-ix.net/en/"
             "https://cloud.dd-ix.net/status.php"
-            "https://dcim.dd-ix.net/api/"
+            "https://dcim.dd-ix.net/"
             "https://lg.dd-ix.net"
             "https://portal.dd-ix.net/login"
             "https://wiki.dd-ix.net/status"
