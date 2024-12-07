38993c58178545d2a8d0e50b3c9bfdfd4d5fe73b
Marcel <me@m4rc3l.de>
2024-12-07 13:12:45 +0100

    svc-hv01: generate networks

--- hosts/svc-hv01/network.nix
+++ hosts/svc-hv01/network.nix
@@ -60,100 +60,80 @@ in
       )))
     ];
 
-    networks = {
-      "10-${bond_device_name}" = {
-        matchConfig.Name = "${bond_device_name}";
+    networks = lib.mkMerge [
+      {
+        "10-${bond_device_name}" = {
+          matchConfig.Name = "${bond_device_name}";
 
-        vlan = [ "${bond_device_name}.100" "${bond_device_name}.101" "${bond_device_name}.102" "${bond_device_name}.103" "${bond_device_name}.104" "${bond_device_name}.301" "${bond_device_name}.601" ];
-      };
-
-      "10-${first_device_name}-${bond_device_name}" = {
-        matchConfig.Name = "${first_device_name}";
-        networkConfig = {
-          Bond = "${bond_device_name}"; # Enslaving to bond 
+          vlan = [ "${bond_device_name}.100" "${bond_device_name}.101" "${bond_device_name}.102" "${bond_device_name}.103" "${bond_device_name}.104" "${bond_device_name}.301" "${bond_device_name}.601" ];
         };
-      };
 
-      "10-${second_device_name}-${bond_device_name}" = {
-        matchConfig.Name = "${second_device_name}";
-        networkConfig = {
-          Bond = "${bond_device_name}"; # Enslaving to bond
+        "10-${first_device_name}-${bond_device_name}" = {
+          matchConfig.Name = "${first_device_name}";
+          networkConfig = {
+            Bond = "${bond_device_name}"; # Enslaving to bond 
+          };
         };
-      };
 
-      "10-${bond_device_name}.100" = {
-        matchConfig.Name = "${bond_device_name}.100";
-        networkConfig.Bridge = "svc-internet";
-      };
-
-      "10-${bond_device_name}.101" = {
-        matchConfig.Name = "${bond_device_name}.101";
-        networkConfig.Bridge = "svc-services";
-      };
-
-      "10-${bond_device_name}.102" = {
-        matchConfig.Name = "${bond_device_name}.102";
-        networkConfig.Bridge = "svc-management";
-      };
-
-      "10-svc-management" = {
-        matchConfig.Name = "svc-management";
-        address = [ "2a01:7700:80b0:7000::2/64" ];
-        routes = [{ routeConfig.Gateway = "fe80::1"; }];
-      };
-
-      "10-${bond_device_name}.103" = {
-        matchConfig.Name = "${bond_device_name}.103";
-        networkConfig.Bridge = "svc-lab";
-      };
-
-      "10-${bond_device_name}.104" = {
-        matchConfig.Name = "${bond_device_name}.104";
-        networkConfig.Bridge = "svc-admin";
-      };
-
-      "10-${bond_device_name}.301" = {
-        matchConfig.Name = "${bond_device_name}.301";
-        networkConfig.Bridge = "svc-ixp-mgmt";
-      };
-
-      "10-${bond_device_name}.601" = {
-        matchConfig.Name = "${bond_device_name}.601";
-        networkConfig.Bridge = "prj-linklab";
-      };
-
-      "10-${ixp_peering_device_name}" = {
-        matchConfig.Name = "${ixp_peering_device_name}";
-        networkConfig.Bridge = "ixp-peering";
-      };
-
-      "40-bring-svc-up-bridges" = {
-        matchConfig.Name = "svc-*";
-        networkConfig = {
-          DHCP = "no";
-          LinkLocalAddressing = "no";
-          KeepConfiguration = "yes";
+        "10-${second_device_name}-${bond_device_name}" = {
+          matchConfig.Name = "${second_device_name}";
+          networkConfig = {
+            Bond = "${bond_device_name}"; # Enslaving to bond
+          };
         };
-      };
 
-      "40-bring-ixp-up-bridges" = {
-        matchConfig.Name = "ixp-*";
-        networkConfig = {
-          DHCP = "no";
-          LinkLocalAddressing = "no";
-          KeepConfiguration = "yes";
+        "10-svc-management" = {
+          matchConfig.Name = "svc-management";
+          address = [ "2a01:7700:80b0:7000::2/64" ];
+          routes = [{ routeConfig.Gateway = "fe80::1"; }];
         };
-      };
 
-      "40-bring-prj-up-bridges" = {
-        matchConfig.Name = "prj-*";
-        networkConfig = {
-          DHCP = "no";
-          LinkLocalAddressing = "no";
-          KeepConfiguration = "yes";
+        "10-${ixp_peering_device_name}" = {
+          matchConfig.Name = "${ixp_peering_device_name}";
+          networkConfig.Bridge = "ixp-peering";
         };
-      };
-    };
+
+        "40-bring-svc-up-bridges" = {
+          matchConfig.Name = "svc-*";
+          networkConfig = {
+            DHCP = "no";
+            LinkLocalAddressing = "no";
+            KeepConfiguration = "yes";
+          };
+        };
+
+        "40-bring-ixp-up-bridges" = {
+          matchConfig.Name = "ixp-*";
+          networkConfig = {
+            DHCP = "no";
+            LinkLocalAddressing = "no";
+            KeepConfiguration = "yes";
+          };
+        };
+
+        "40-bring-prj-up-bridges" = {
+          matchConfig.Name = "prj-*";
+          networkConfig = {
+            DHCP = "no";
+            LinkLocalAddressing = "no";
+            KeepConfiguration = "yes";
+          };
+        };
+      }
+      (builtins.listToAttrs (lib.flatten (
+        lib.mapAttrsToList
+          (name: value: [
+            {
+              name = "10-${bond_device_name}.${builtins.toString value.vlan}";
+              value = {
+                matchConfig.Name = "${bond_device_name}.${builtins.toString value.vlan}";
+                networkConfig.Bridge = value.bridge;
+              };
+            }
+          ])
+          config.dd-ix.nets
+      )))
+    ];
   };
 
   # enabling and configuring firewall
