8cda53361966969374ba8eade8513573def6b76b
Marcel <me@m4rc3l.de>
2024-12-21 21:31:19 +0100

    sops: remove expired keys

--- modules/management/weblate.nix
+++ modules/management/weblate.nix
@@ -1,9 +1,15 @@
-{ lib, config, ... }:
+{self, lib, config, ... }:
 
 {
+  sops.secrets."weblate/django_secret_key" = {
+    sopsFile = self + /secrets/management/translate.yaml;
+    owner = config.systemd.services.weblate.serviceConfig.User;
+  };
+
   services.weblate = {
     enable = true;
     localDomain = "weblate.${config.dd-ix.domain}";
+    djangoSecretKeyFile = config.sops.secrets."weblate/django_secret_key".path;
     smtp = {
       enable = true;
       host = "svc-mta01.dd-ix.net";
