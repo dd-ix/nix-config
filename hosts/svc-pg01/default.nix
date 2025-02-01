3a305bb4d4e8181f90a639b2074b86dbba70d65c
Marcel <me@m4rc3l.de>
2025-02-01 22:55:50 +0100

    svc-pg01: cleanup

--- hosts/svc-pg01/default.nix
+++ hosts/svc-pg01/default.nix
@@ -1,6 +1,25 @@
-{ ... }: {
+{
   imports = [
-    ./configuration.nix
-    ../../modules/management/postgres.nix
+    ./postgres.nix
   ];
+
+  dd-ix = {
+    useFpx = true;
+    hostName = "svc-pg01";
+
+    microvm = {
+      enable = true;
+
+      mem = 2048;
+      vcpu = 2;
+    };
+
+    acme = [{ name = "svc-pg01.dd-ix.net"; }];
+
+    monitoring = {
+      enable = true;
+    };
+  };
+
+  system.stateVersion = "23.11";
 }
