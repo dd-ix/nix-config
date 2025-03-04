933139e35c30a028bd6ff50801975ef4064306ca
Marcel <me@m4rc3l.de>
2025-03-04 21:07:51 +0100

    nms: add weathermap plugin

--- modules/management/librenms.nix
+++ modules/management/librenms.nix
@@ -1,4 +1,14 @@
-{ self, config, ... }:
+{ self, lib, config, pkgs, ... }:
+
+let
+  weathermap = pkgs.fetchFromGitHub {
+    owner = "librenms-plugins";
+    repo = "Weathermap";
+    # master as of 04.03.2025
+    rev = "ea57b454eb042408a6628fc3d8dff8176563547f";
+    hash = "sha256-lTeyxzJNQeMdu1IVdovNMtgn77jRIhSybLdMbTkf2W1=";
+  };
+in
 
 {
   sops.secrets."nms_db_pass" = {
@@ -8,6 +18,12 @@
 
   services.librenms = {
     enable = true;
+    package = pkgs.librenms.override {
+      plugins = [{
+        dir = weathermap;
+        name = "Weathermap";
+      }];
+    };
     database = {
       host = "svc-mari01.dd-ix.net";
       port = 3306;
@@ -26,6 +42,28 @@
 
       onlySSL = true;
       useACMEHost = "nms.${config.dd-ix.domain}";
+    };
+  };
+
+  systemd = {
+    services.librenms-weathermap-poller = {
+      description = "LibreNMS Weathermap Poller";
+
+      serviceConfig = {
+        Type = "oneshot";
+        inherit (config.systemd.services.librenms-scheduler.serviceConfig) WorkingDirectory User Group;
+        ExecStart = "${lib.getExe config.services.librenms.package.passthru.phpPackage} plugins/Weathermap/map-poller.php";
+      };
+    };
+
+    timers.librenms-scheduler = {
+      description = "LibreNMS Weathermap Poller";
+      wantedBy = [ "timers.target" ];
+      timerConfig = {
+        # every 5min
+        OnCalendar = "*:0/5";
+        AccuracySec = "1second";
+      };
     };
   };
 }
