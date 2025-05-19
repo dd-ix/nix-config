81d150b68b9eff541b3bef70a6732c1927058a6b
Marcel <me@m4rc3l.de>
2025-05-19 14:37:07 +0200

    modules/common: fix zfs kernel selection

--- modules/common/kernel.nix
+++ modules/common/kernel.nix
@@ -1,28 +1,5 @@
-{ lib, pkgs, config, ... }:
+{ lib, pkgs, ... }:
 
-let
-  # original source: https://github.com/nix-community/srvos/blob/main/nixos/mixins/latest-zfs-kernel.nix
-  isUnstable = config.boot.zfs.package == pkgs.zfsUnstable;
-  zfsCompatibleKernelPackages = lib.filterAttrs
-    (
-      name: kernelPackages:
-        (builtins.match "linux_[0-9]+_[0-9]+" name) != null
-        && (builtins.tryEval kernelPackages).success
-        && (
-          (!isUnstable && !kernelPackages.zfs.meta.broken)
-          || (isUnstable && !kernelPackages.zfs_unstable.meta.broken)
-        )
-    )
-    pkgs.linuxKernel.packages;
-  latestZfsKernelPackage = lib.last (
-    lib.sort (a: b: (lib.versionOlder a.kernel.version b.kernel.version)) (
-      builtins.attrValues zfsCompatibleKernelPackages
-    )
-  );
-  zfsEnabled = lib.meta.availableOn pkgs.hostPlatform pkgs.zfs
-    && config.boot.zfs.enabled;
-in
 {
-  # Note this might jump back and worth as kernel get added or removed.
-  boot.kernelPackages = if zfsEnabled then latestZfsKernelPackage else pkgs.linuxPackages_latest;
+  boot.kernelPackages = lib.mkDefault pkgs.linuxPackages_latest;
 }
--- modules/common/zfs.nix
+++ modules/common/zfs.nix
@@ -1,8 +1,30 @@
 { lib, config, pkgs, ... }:
 
+let
+  zfsVersion = "2_2";
+  # original source: https://github.com/nix-community/srvos/blob/main/nixos/mixins/latest-zfs-kernel.nix
+  zfsCompatibleKernelPackages = lib.filterAttrs
+    (
+      name: kernelPackages:
+        (builtins.match "linux_[0-9]+_[0-9]+" name) != null
+        && (builtins.tryEval kernelPackages).success
+        && (!kernelPackages."zfs_${zfsVersion}".meta.broken)
+    )
+    pkgs.linuxKernel.packages;
+  latestZfsKernelPackage = lib.last (
+    lib.sort (a: b: (lib.versionOlder a.kernel.version b.kernel.version)) (
+      builtins.attrValues zfsCompatibleKernelPackages
+    )
+  );
+in
 {
+
   config = lib.mkIf config.boot.zfs.enabled {
-    boot.zfs.package = pkgs.zfs_2_2;
+    boot = {
+      # Note this might jump back and worth as kernel get added or removed.
+      kernelPackages = latestZfsKernelPackage;
+      zfs.package = pkgs."zfs_${zfsVersion}";
+    };
 
     services.zfs = {
       autoSnapshot = {
