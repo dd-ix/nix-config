8b19d8daf995fbb0a706b687a347b83de4f85668
Marcel <me@m4rc3l.de>
2025-06-18 22:20:56 +0200

    acme: always configure global config

--- modules/dd-ix/acme.nix
+++ modules/dd-ix/acme.nix
@@ -18,41 +18,45 @@ in
     default = [ ];
   };
 
-  config = lib.mkIf ((lib.length cfg) != 0) {
-    security.acme = {
-      acceptTerms = true;
-      defaults.email = "noc@dd-ix.net";
-    };
+  config = lib.mkMerge [
+    {
+      security.acme = {
+        acceptTerms = true;
+        defaults.email = "noc@dd-ix.net";
+      };
+    }
+    (lib.mkIf ((lib.length cfg) != 0) {
 
-    sops.secrets = lib.listToAttrs (map
-      (domain: {
-        name = "rfc2136_${domain.name}";
-        value = {
-          sopsFile = self + "/secrets/management/rfc2136/${domain.name}.yaml";
-          owner = "root";
-        };
-      })
-      cfg);
+      sops.secrets = lib.listToAttrs (map
+        (domain: {
+          name = "rfc2136_${domain.name}";
+          value = {
+            sopsFile = self + "/secrets/management/rfc2136/${domain.name}.yaml";
+            owner = "root";
+          };
+        })
+        cfg);
 
-    security.acme.certs = lib.listToAttrs (map
-      (domain: {
-        name = "${domain.name}";
-        value = {
-          dnsProvider = "rfc2136";
-          group = domain.group;
-          credentialsFile = config.sops.secrets."rfc2136_${domain.name}".path;
-        };
-      })
-      cfg);
+      security.acme.certs = lib.listToAttrs (map
+        (domain: {
+          name = "${domain.name}";
+          value = {
+            dnsProvider = "rfc2136";
+            group = domain.group;
+            credentialsFile = config.sops.secrets."rfc2136_${domain.name}".path;
+          };
+        })
+        cfg);
 
-    systemd.services = lib.listToAttrs (map
-      (domain: {
-        name = "acme-${domain.name}";
-        value.environment = lib.mkIf config.dd-ix.useFpx {
-          http_proxy = config.networking.proxy.httpProxy;
-          https_proxy = config.networking.proxy.httpsProxy;
-        };
-      })
-      cfg);
-  };
+      systemd.services = lib.listToAttrs (map
+        (domain: {
+          name = "acme-${domain.name}";
+          value.environment = lib.mkIf config.dd-ix.useFpx {
+            http_proxy = config.networking.proxy.httpProxy;
+            https_proxy = config.networking.proxy.httpsProxy;
+          };
+        })
+        cfg);
+    })
+  ];
 }
