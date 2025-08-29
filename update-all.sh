d3600a15eb31dd031131a729a50d03ace6186e56
Marcel <me@m4rc3l.de>
2025-08-29 20:26:13 +0200

    update-all: fix sleep unit
    is seconds and not milliseconds

--- update-all.sh
+++ update-all.sh
@@ -3,7 +3,7 @@
 # critical VMs, when these are not running the others may not boot or build correctly
 microvm -Ru svc-fpx01
 # fpx may need some time to finish building
-sleep 5000
+sleep 5
 
 microvm -Ru svc-pg01
 microvm -Ru svc-mari01
