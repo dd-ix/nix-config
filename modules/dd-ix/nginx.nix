62f4c05fd3b83cce98b0670736c07c38a7913848
Marcel <me@m4rc3l.de>
2025-11-06 15:31:28 +0100

    dd-ix: fix nginx config

--- modules/dd-ix/nginx.nix
+++ modules/dd-ix/nginx.nix
@@ -1,34 +1,10 @@
 { lib, config, ... }:
 
 let
-  enable = config.services.nginx.enable;
-  headers = ''
-    # Permissions Policy - gps only
-    more_set_headers "Permissions-Policy: geolocation=()";
-
-    # Minimize information leaked to other domains
-    more_set_headers "Referrer-Policy: origin-when-cross-origin";
-
-    # Disable embedding as a frame
-    # add_header X-Frame-Options DENY;
-
-    # Prevent injection of code in other mime types (XSS Attacks)
-    #add_header X-Content-Type-Options nosniff;
-
-    # Enable XSS protection of the browser.
-    # May be unnecessary when CSP is configured properly (see above)
-    #add_header X-XSS-Protection "1; mode=block";
-
-    # STS
-    more_set_headers "Strict-Transport-Security: max-age=31536000; includeSubDomains";
-
-    # real ip
-    set_real_ip_from 2a01:7700:80b0:6000::443;
-    real_ip_header proxy_protocol;
-  '';
+  cfg = config.services.nginx;
 in
 {
-  networking.firewall.allowedTCPPorts = lib.mkIf enable [ 80 443 ];
+  networking.firewall.allowedTCPPorts = lib.mkIf cfg.enable [ 80 443 ];
   services = {
     nginx = {
       recommendedZstdSettings = true;
@@ -37,9 +13,12 @@ in
       recommendedOptimisation = true;
       recommendedGzipSettings = true;
       recommendedBrotliSettings = true;
-      commonServerConfig = headers;
+      commonHttpConfig = ''
+        set_real_ip_from 2a01:7700:80b0:6000::443;
+        real_ip_header proxy_protocol;
+      '';
     };
-    prometheus.exporters.nginxlog = lib.mkIf enable {
+    prometheus.exporters.nginxlog = lib.mkIf cfg.enable {
       enable = true;
       group = "nginx";
       openFirewall = true;
